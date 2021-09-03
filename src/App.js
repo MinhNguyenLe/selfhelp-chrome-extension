1fa8388a05b3c576ce2d7a95256fcc251ca5d8a6
MinhNguyenLe <minh.nguyenle1809@gmail.com>
2021-09-03 18:20:58 +0700

    set up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello there!</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
