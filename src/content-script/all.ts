81e8a42a1b8cbfca6db796d5937893eec5afa1b7
MinhNguyenLe <minh.nguyenle1809@gmail.com>
2021-10-01 21:08:58 +0700

    fix hidden sider

--- src/content-script/all.ts
+++ src/content-script/all.ts
@@ -1,5 +1,5 @@
 document.getElementById("pre-pag-ads")?.setAttribute("style", "display : none");
-document.getElementById("sidebar")?.setAttribute("style", "display : none");
+// document.getElementById("sidebar")?.setAttribute("style", "display : none");
 document
   .getElementById("ad-container")
   ?.setAttribute("style", "display : none");
